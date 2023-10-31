45d13b9cfdfdf2e80da41da6426e7e97852057b9
James Dominguez <69368392+JamesMDominguez@users.noreply.github.com>
2023-10-31 02:11:24 -0700

    style

--- app/page.jsx
+++ app/page.jsx
@@ -283,12 +283,12 @@ export default function Home() {
 
           <Stack direction={"row"}>
             <Stack sx={{ width: 80 }}>
-              <div style={{ borderStyle: "solid", height: '1.4rem', borderWidth: "thin" }}>
+              <div style={{ borderStyle: "solid", height: '1.4rem', borderWidth: "thin",borderTopLeftRadius: '10px' }}>
                 <p style={{ fontSize: "14px", textAlign: "center", margin: 0 }}>Time</p>
               </div>
               {timeRange.map((i) => {
                 return (
-                  <div key={"Weekly" + i} style={{ color: '#5d5e5e', fontSize: "14px", borderStyle: "solid", borderWidth: "thin", textAlign: "center", height: '1.3rem' }} >
+                  <div key={"Weekly" + i} style={{borderBottomLeftRadius:i=='7:00pm'?'10px':0, color: '#5d5e5e', fontSize: "14px", borderStyle: "solid", borderWidth: "thin", textAlign: "center", height: '1.3rem' }} >
                     <p style={{ fontSize: "14px", textAlign: "center", margin: 0 }}>{i}</p>
                   </div>
                 )
@@ -300,12 +300,12 @@ export default function Home() {
               {days.map((day) => {
                 return (
                   <Stack key={"urDay" + day} sx={{ maxWidth: 120 }}>
-                    <div style={{ borderStyle: "solid", height: '1.4rem', borderWidth: "thin" }}>
+                    <div style={{ borderStyle: "solid", height: '1.4rem', borderWidth: "thin",borderTopRightRadius: day=='Fri'?'10px':0 }}>
                       <p style={{ margin: "0", fontSize: "14px", textAlign: "center" }} >{day}</p>
                     </div>
                     {timeRange.map((i, index) => {
                       return (
-                        <select name={day + "_" + i} onChange={handleTimeSelect} key={"timerange" + i} style={{ maxWidth: 90, width: 80, height: '1.425rem', backgroundColor: grayOutBox(day, index) ? '#c3c4c7' : '' }}>
+                        <select name={day + "_" + i} onChange={handleTimeSelect} key={"timerange" + i} style={{borderBottomRightRadius:i=='7:00pm'&&day=='Fri'?'10px':0, maxWidth: 90, width: 80, height: '1.425rem', backgroundColor: grayOutBox(day, index) ? '#c3c4c7' : '' }}>
                           <option value="N/A"></option>
                           <option value="In-Person">In-Person</option>
                           <option value="Remote">Remote</option>
